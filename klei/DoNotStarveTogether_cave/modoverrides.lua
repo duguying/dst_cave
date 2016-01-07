6abe6698999994a79ceadaebc4de0a291e9529d2
Rex Lee(李俊) <duguying2008@gmail.com>
2016-01-07 16:22:42 +0800

    klei setting

---
+++ klei/DoNotStarveTogether_cave/modoverrides.lua
@@ -0,0 +1,8 @@
+return {
+    ["workshop-437711916"] = { enabled = true },
+    ["workshop-458940297"] = { enabled = true },
+    ["workshop-378160973"] = { enabled = true },
+    ["workshop-375859599"] = { enabled = true },
+    ["workshop-447092740"] = { enabled = true },
+    ["workshop-375850593"] = { enabled = true }
+}
